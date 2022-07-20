3f9c8f1e014d6bdf2a986cc99ecb586b11934b1c
Gaurav <csogoyal@gmail.com>
2022-07-20 06:15:30 -0400

    partial crud

--- proj4/server.js
+++ proj4/server.js
@@ -1,27 +1,24 @@
 import express from 'express'
 const app = express()
-import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 import { Router } from 'express'
-
-
 import connectDB from './db/connect.js'
 import reviewRouter from './routes/reviewRoutes.js'
 import authRouter from './routes/authRoutes.js'
-
+import cors from 'cors'
 import notFoundMiddleware from  './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-// app.use(cors({origin: "*"}))
 
-app.use((req,res,next)=>{
-  console.log(req.url);
-  console.log(req.method);
-  next();
-});
+app.use(cors())
+// app.use((req,res,next)=>{
+//   console.log(req.url);
+//   console.log(req.method);
+//   next();
+// });
 app.get('/', (req, res) => {
   // throw new Error('error2')
   res.send('Welcome!')
@@ -36,19 +33,23 @@ app.use(errorHandlerMiddleware)
 
 
 const port = process.env.PORT || 5000
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`)
+
+connectDB.on("connected", ()=>{
+  
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`)
+  })
 })
 
-const start = async () => {
-    try {
-      await connectDB(process.env.MONGO_URL)
-      app.listen(port, () => {
-        console.log(`Server is listening on port ${port}...`)
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  }
+// const start = async () => {
+//     try {
+//       await connectDB(process.env.MONGO_URL)
+//       app.listen(port, () => {
+//         console.log(`Server is listening on port ${port}...`)
+//       })
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
   
-  start()
+//   start()
